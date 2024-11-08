79b195c8eaa616da8c380cdecb6b040bc9a5484c
Trey Zima <trey.m.zima@gmail.com>
2024-11-08 16:58:34 -0700

    Updated README and created working files

---
+++ promises.js
@@ -0,0 +1,2 @@
+// Part 1: Number Facts
+// 1. Make a request to the Numbers API http://numbersapi.com/ to get a fact about your favorite number. (Make sure you get back JSON by including the json query key, specific to this API.
